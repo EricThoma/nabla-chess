c1849b0a1235122439ade86a4afa68de13183caa
Eric Thoma <ethoma7329@gmail.com>
2014-12-23 21:56:27 -0600

    Added comments, deleted dead code
    For my own sanity. A couple functions that were out of use/useless were
    deleted.

--- Magic.hs
+++ Magic.hs
@@ -1,13 +1,15 @@
+-- in theory I need only to reveal the first three functions for the rest of the engine
+-- more is exported now for testing purposes, convenience, etc.
 module Magic (
+magicBishopMoves,
+magicRookMoves,
+magicQueenMoves,
 bishopMovesNoLookup,
 rookMovesNoLookup,
 genOccupancies,
 bishopMasks,
 rookMasks,
 genMagic,
-magicBishopMoves,
-magicRookMoves,
-magicQueenMoves,
 bishopMagics,
 rookMagics,
 getRookMask
@@ -22,6 +24,7 @@ import Data.Maybe
 import Data.Bits
 import Debug.Trace
 
+-- magic bitboard bishop interface
 magicBishopMoves :: BB -> Int -> BB
 magicBishopMoves occ sq = 
                           let MagicEntry mask magic shift lookup = bishopMagics Array.! sq
@@ -30,6 +33,7 @@ magicBishopMoves occ sq =
                               ind = (fromIntegral $ opm `unsafeShiftR` shift)::Int
                           in lookup UArray.! ind
 
+-- magic bitboard rook interface
 magicRookMoves :: BB -> Int -> BB
 magicRookMoves occ sq = 
                           let MagicEntry mask magic shift lookup = rookMagics Array.! sq
@@ -38,21 +42,24 @@ magicRookMoves occ sq =
                               ind = (fromIntegral $ opm `unsafeShiftR` shift)::Int
                           in lookup UArray.! ind
 
+-- composition of the above two for queen moves, interface
 magicQueenMoves :: BB -> Int -> BB
 magicQueenMoves occ sq = (magicBishopMoves occ sq) .|. (magicRookMoves occ sq)
 
+-- list of bishop magics for each square
 bishopMagics :: Array Int MagicEntry
 bishopMagics = Array.listArray (0,63) [genMagic True i | i <- [0..63]]
 
+-- list of rook magics for each square
 rookMagics :: Array Int MagicEntry
 rookMagics = Array.listArray (0,63) [genMagic False i | i <- [0..63]]
 
-magicsSquare :: Int -> MagicEntry
-magicsSquare sq = let entry = randomEntry sq
-				  in case entry of
-				  	    Just x -> x
-				  	    Nothing -> magicsSquare sq
-
+-- generate a magic number for the square
+-- process is as follows: 
+-- generate masks
+-- generate all possible mask occupancies
+-- plug those occupancies into the old-fashioned move generation for attack maps
+-- generate a magic number that is correct via tryMagics
 genMagic :: Bool -> Int -> MagicEntry
 genMagic bishop sq = let mask = if bishop
                                    then bishopMasks UArray.! sq
@@ -69,6 +76,7 @@ genMagic bishop sq = let mask = if bishop
                         --then let mask = bishopMasks UArray.! sq
                         --else let mask = bishopMasks UArray.! sq
 
+-- try out magics until one works, use sparse random numbers for the magics
 tryMagics :: StdGen -> UArray Int BB -> [BB] -> Int -> (BB,[(Int, BB)])
 tryMagics gen attmap occs indexBits = let usedmap = (UArray.listArray (0,(bit indexBits)-1) (replicate (bit indexBits) False))::UArray Int Bool
                                           (magic, ngen) = sparseRand gen
@@ -77,22 +85,22 @@ tryMagics gen attmap occs indexBits = let usedmap = (UArray.listArray (0,(bit in
                                             then (magic,res)
                                             else tryMagics ngen attmap occs indexBits
 
+-- worker function for try magics; sees if a given magic number works
+-- process: compute the index from the given magic and verify that index isn't used yet
 genMagicW :: UArray Int BB -> UArray Int Bool -> [BB] -> Int -> Int -> BB -> [(Int,BB)] -> ([(Int, BB)], Bool)
 genMagicW _ _ [] _ _ _ accum = (accum,True)
 genMagicW attmap usedmap (occ:occs) occno indexBits magic accum =
           let index = (fromIntegral $ (occ*magic) `shiftR` (64-indexBits))::Int
-              --index = trace (show $ UArray.indices attmap) (fromIntegral $ (occ*magic) `shiftR` (64-indexBits))::Int
           in if (usedmap UArray.! index)
                 then (accum,False)
                 else genMagicW attmap (usedmap UArray.// [(index,True)]) occs (occno+1) indexBits magic ((index, attmap UArray.! occno):accum)
-                
-                             
-                        --else MagicEntry 0 0 0 (UArray.listArray (0,1) [])
 
+-- generate all occupancies in a given mask, square
 genOccupancies :: BB -> Int -> [BB]
 genOccupancies mask sq = let nbits = popCount mask
                          in genOccupanciesW mask nbits sq 0 []
 
+-- worker function for genOccupancies
 genOccupanciesW :: BB -> Int -> Int -> Int -> [BB] -> [BB]
 genOccupanciesW mask nbits sq iter accum = if iter >= (bit nbits)
                                               then accum
@@ -108,16 +116,16 @@ getOcc mask nbits i j occ = if j >= nbits
                                                else occ
                                     in getOcc nmask nbits i (j+1) nocc
 
-
-randomEntry :: Int -> Maybe MagicEntry
-randomEntry sq = Nothing
-
+-- list of bishop masks (bishop moves on an empty board minus edges)
+-- the hex number is simply the board minus edges
 bishopMasks :: UArray Int BB
 bishopMasks = UArray.listArray (0,63) [(0x007E7E7E7E7E7E00::BB) .&. (bishopMovesNoLookup 0 i) | i <- [0..63]]
 
+-- list of the rook masks (rook moves on an empty board minus edges)
 rookMasks :: UArray Int BB
 rookMasks = UArray.listArray (0,63) [getRookMask i | i <- [0..63]]
 
+-- compute the rook mask for a given square
 getRookMask :: Int -> BB
 getRookMask sq = let (r,f) = linToRF sq
                      n = \(ri, fi) -> (ri+1, fi, ri+1 < 6)
@@ -126,6 +134,9 @@ getRookMask sq = let (r,f) = linToRF sq
                     .|. genWrapper e (r, 0, True) 0)
                     .&. (complement $ bit sq)
 
+-- computes naively the bishop moves by defining directions and calling a generic looping function
+-- simplification proposal:
+-- make a direction by specifying a row/col funcs Int->Int and have generic bound conditions
 bishopMovesNoLookup :: BB -> Int -> BB
 bishopMovesNoLookup occ sq = let (r, f) = linToRF sq
                                  ne = \(ri, fi) -> (ri+1, fi+1, ri+1<7 && fi+1<7 && (not $ testBit occ $ rfToLin (ri+1) (fi+1)))
@@ -138,6 +149,7 @@ bishopMovesNoLookup occ sq = let (r, f) = linToRF sq
                                  .|. genWrapper se (r, f, r>0 && f<7) 0
                                  .|. genWrapper sw (r, f, r>0 && f>0) 0
 
+-- computes naively the rook moves by defining directions and calling a generic looping function
 rookMovesNoLookup :: BB -> Int -> BB
 rookMovesNoLookup occ sq = let (r, f) = linToRF sq
                                n = \(ri, fi) -> (ri+1, fi, ri+1 < 7 && (not $ testBit occ $ rfToLin (ri+1) (fi)))
@@ -149,6 +161,7 @@ rookMovesNoLookup occ sq = let (r, f) = linToRF sq
                                .|. genWrapper s (r, f, r>0) 0
                                .|. genWrapper e (r, f, f<7) 0
 
+-- generic looping function that iterates over sliding moves, used for naive move generation
 genWrapper :: ((Int, Int) -> (Int, Int, Bool)) -> (Int, Int, Bool) -> BB -> BB
 genWrapper func (_, _, False) att = att
 genWrapper func (rank, file, True) att = let (rn, fn, b) = func (rank,file)
